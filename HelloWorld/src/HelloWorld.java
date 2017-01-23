f0776ef0ae2283d5fbb688388e24c0cc421088a0
Peggy <Peggy@>
2017-01-23 11:15:29 +0800

    test

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -14,11 +14,9 @@ public class HelloWorld {
 		System.out.println("Test 1");
 		System.out.println("Test 2");
 		System.out.println("Test 2");
-<<<<<<< Upstream, based on origin/CR3
+
 		System.out.println("Test 2");
-=======
 		System.out.println("Test 3");
->>>>>>> ca86f57 test fetch3
 	}
 
 }
