67288110052882b131c7c501749e18ac86d879a8
Peggy <Peggy@>
2017-03-15 15:04:55 +0800

    test cherry pick
    Conflicts:
    	HelloWorld/src/HelloWorld.java

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -10,18 +10,12 @@ public class HelloWorld {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
 		System.out.println("Test 1");
-<<<<<<< Upstream, based on origin/CR3
 		System.out.println("Test 1");
 		System.out.println("Test 1");
 		System.out.println("Test 2");
 		System.out.println("Test 2");
-
-		System.out.println("Test 2");
-		System.out.println("Test 3");
-		System.out.println("Test 5");
-=======
-		System.out.println("Update master");
->>>>>>> c415f38 Update master branch
+		
+		System.out.println("Cherry Pick");
 	}
 
 }
